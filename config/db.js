b1c983d9d96b53eab941884c734147e7e9dd1773
Dávid Lajos Mihalovits <dmihalovits@gmail.com>
2021-04-06 11:19:29 +0200

    new

--- config/db.js
+++ config/db.js
@@ -1,16 +1,33 @@
-const { Sequelize } = require("sequelize");
+/*const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const dev = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const prod = process.env.DATABASE_URL; // heroku Postgres addon
 
-module.exports = new Sequelize(
+module.exports = new Client(
     process.env.NODE_ENV === "production" ? prod : dev,
     { logging: false }
-);
-
-/*module.exports = new Sequelize(
-    "postgres://postgres:password@localhost:5432/remotedev",
-    { logging: false }
 );*/
+
+const { Client } = require("pg");
+
+module.exports = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+client.connect();
+
+client.query(
+    "SELECT table_schema,table_name FROM information_schema.tables;",
+    (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+            console.log(JSON.stringify(row));
+        }
+        client.end();
+    }
+);
